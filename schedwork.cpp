6d1972cbe27a669efcedd60482dab4ae1d3ff279
Lingli Feng <linglife@usc.edu>
2023-04-07 00:19:01 +0000

    Initial commit for hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleH(const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, 
+std::vector<size_t> shiftCount);
+bool specialRow(const DailySchedule& avail, int r );
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,8 +39,62 @@ bool schedule(
     sched.clear();
     // Add your code below
 
-
-
-
+    std::vector<size_t> shiftCount; 
+    for (size_t row = 0; row < avail.size(); row++)
+    {
+        sched.push_back(std::vector<Worker_T>());
+        for (int col = 0; col < dailyNeed; col++)
+        {
+            sched[row].push_back(-1);
+        }
+    }
+    for (size_t i = 0; i < avail[0].size(); i++)
+    {
+        shiftCount.push_back(0);
+    }
+    return scheduleH(avail, maxShifts, sched, shiftCount);
 }
 
+bool scheduleH(const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, std::vector<size_t> shiftCount)
+{
+    for(size_t row = 0; row < sched.size(); row++)
+    {
+        for(size_t col = 0; col < sched[row].size(); col++)
+        {
+            if (sched[row][col] == -1)
+            {
+                for(size_t worker = 0; worker < avail[0].size(); worker++)
+                {
+                    if(avail[row][worker] && shiftCount[worker] < maxShifts)
+                    {
+                        sched[row][col] = worker; 
+                        ++shiftCount[worker];
+                        if(specialRow(sched, row) && scheduleH(avail, maxShifts, sched, shiftCount))
+                        {
+                            return true;
+                        }
+                        sched[row][col] = -1;
+                        --shiftCount[worker];
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool specialRow(const DailySchedule& sched, int r)
+{
+    std::map<int, bool> workers; 
+    for(size_t i = 0; i < sched[r].size(); ++i)
+    {
+        if(sched[r][i] == -1)
+            continue; 
+        else if (workers.find(sched[r][i]) != workers.end())
+            return false; 
+        
+        workers.insert(std::make_pair(sched[r][i], true));
+    }
+    return true; 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -12,8 +12,10 @@
 using namespace std;
 
 
-// Add prototypes of helper functions here
 
+// Add prototypes of helper functions here
+void findWord(const std::string& in, const std::string& floating, const std::set<std::string>& dict, 
+              int index, int length, std::set<std::string>& possibleGusse );
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,93 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    int length = in.size();
+    // std::string inCopy = in;
+	// std::string floattingCopy = floating;
+	// std::set<std::string> dictCopy = dict;
+    std::set<std::string> possibleGusse;
+     findWord(in, floating, dict, 0, length, &possibleGusse);
+    return possibleGusse;
 }
 
 // Define any helper functions here
+void findWord(const std::string& in, const std::string& floating, const std::set<std::string>& dict, 
+              int index, int dashes, std::set<std::string>& possibleGusse )
+{   
+
+        if(index == in.size())
+		{
+			 if(dict.find(in) != dict.end() && floating.empty())
+			 {
+				 possibleGusse.insert(in);
+			 }
+			 return;
+		}
+}
+    
+     if (in[index - 1] != '-')
+    {
+        findWord(in, floating, dict, index + 1, length, possibleGusse);
+        return;
+    }
+    
+    int dashes = 0;
+    for (int i = index - 1; i < length; i++)
+    {
+        if (in[i] == '-')
+        {
+            dashes++;
+        }
+    }
+    
+    if (index < floating.size())
+    {
+        return;
+    }
+    
+    if (index == floating.size())
+    {
+        string currentIn;
+        for (int i = 0; i < floating.size(); i++)
+        {
+            string currentFloating = floating;
+            currentIn = in;
+            currentIn[index - 1] = currentFloating[i];
+            currentFloating.erase(currentFloating.begin() + i);
+            findWord(currentIn, currentFloating, dict, index + 1, length, possibleGusse);
+        }
+        currentIn[index - 1] = '-';
+    }
+    else
+    {
+        std::set<char> floatCharacter;
+        string currentIn;
+        if (floating.size() != 0)
+        {
+            for (int i = 0; i < floating.size(); i++)
+            {
+                string currentFloating = floating;
+                currentIn = in;
+                currentIn[index - 1] = currentFloating[i];
+                currentFloating.erase(currentFloating.begin() + i);
+                floatCharacter.insert((char)(i + 97));
+                findWord(currentIn, currentFloating, dict, index + 1, length, possibleGusse);
+            }
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (floatCharacter.find((char)(i + 97)) != floatCharacter.end())
+            {
+                continue;
+            }
+            else
+            {
+                currentIn = in;
+                currentIn[index - 1] = (char)(i + 97);
+                findWord(currentIn, currentFloating, dict, index + 1, length, possibleGusse);
+            }
+        }
+        currentIn[index - 1] = '-';
+    }
+}
+
