8a1bbbcb4e3541e28e29bb7f586cc69a7e4314bc
Lingli Feng <linglife@usc.edu>
2023-04-07 01:13:07 +0000

    2 try

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,12 +20,63 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 //Add prototypes for any helper functions here
-bool scheduleH(const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, 
-std::vector<size_t> shiftCount);
-bool specialRow(const DailySchedule& avail, int r );
+bool helpNurse(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<unsigned int> track);
 
 // Add your implementation of schedule() and other helper functions here
-
+bool helpNurse(const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, std::vector<unsigned int> track)
+{
+	//sched.size()-1 = day
+	//base case
+	//if you reach end without backtracking, then schedule is good
+	//avail.size = orkers' availability over an n-day period
+	if ((sched.size()-1) >= avail.size())
+	{
+		//if there is extra day, then get rid of extra day
+		if (sched[sched.size()-1].size() == 0) {
+				sched.pop_back();
+		}
+		return true;
+	}
+	//days start with 0
+	//checking if dailyneed of nurses has been met
+	if (sched[sched.size()-1].size() == dailyNeed)
+	{
+		//move onto the next day
+		std::vector<Worker_T> empty;
+		//oushback = adding another day to schedule
+		sched.push_back(empty);
+		return helpNurse(avail,dailyNeed,maxShifts,sched,track);
+	}
+	//count nurser's available on that day
+	for(unsigned int i = 0; i < avail[0].size(); i++)
+	{
+		//checking if nurse [i] is available and they work less than max # of shifts
+		if (avail[sched.size()-1][i] == true && track[i] < maxShifts)
+		{
+			std::vector<unsigned int> copyTrack(track);
+			copyTrack[i]++;
+			//scheduling nurse temporarily
+			sched[sched.size()-1].push_back(i);
+			AvailabilityMatrix copyAvail = avail;
+			//can't be schedule for same day again
+			copyAvail[sched.size()-1][i] = false;
+			//run through each day
+			//if recursive true
+			if(helpNurse(copyAvail,dailyNeed,maxShifts,sched,copyTrack))
+			{
+				return true;
+			}
+			//reverse what we did to try a different nurse
+			sched[sched.size()-1].pop_back();
+		}
+	}
+	//dailyneed = how many nurses are needed
+	//if there are less nurses avaiable than daily need, return false
+	if (sched[sched.size()-1].size() == 0) {
+		sched.pop_back();
+	}
+	return false; 
+}
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -33,25 +84,21 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
-    // Add your code below
-
-    std::vector<size_t> shiftCount; 
-    for (size_t row = 0; row < avail.size(); row++)
-    {
-        sched.push_back(std::vector<Worker_T>());
-        for (int col = 0; col < dailyNeed; col++)
-        {
-            sched[row].push_back(-1);
-        }
-    }
-    for (size_t i = 0; i < avail[0].size(); i++)
-    {
-        shiftCount.push_back(0);
-    }
-    return scheduleH(avail, maxShifts, sched, shiftCount);
+	if(avail.size() == 0U){
+			return false;
+	}
+	sched.clear();
+	// Add your code below
+	//push empty vector
+	//schedule now has at least one day
+	std::vector<Worker_T> empty;
+	sched.push_back(empty);
+	std::vector<unsigned int> track; 
+	for (unsigned int i =0; i < avail[0].size();i++)
+	{
+		//that each nurse has been scheduled for 0 days, beginning
+		track.push_back(0);
+	}
+	return helpNurse(avail, dailyNeed, maxShifts, sched, track);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void findWord(int index,string& in, int dashes, std::string& floating, const std::set<std::string>& dict,
-const std::string& seeds, std::set<std::string>& possibleGusse);
+void wordleHelper(const std::string& in, const std::string& floating,
+                  const std::set<std::string>& dict, int pos, int len, std::set<std::string>* words);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,93 +23,99 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-		std::string current = in;
-		std::string floattingCopy = floating;
-		std::set<std::string> dictCopy = dict;
-    std::set<std::string> word;
-		std::set<std::string> possibleGusse;
-
-    int dashes;
-			for(int i = 0;i < in.size();i++)
-			{
-					if(in[i] == '-')
-					dashes++;
-			}
-		std::string seeds = "abcdefghijklmnopqrstuvwxyz";
-    findWord(0,current,dashes, floattingCopy, dictCopy, seeds, possibleGusse);
-    return possibleGusse;
+    int length = in.size();
+    std::set<std::string> wordSet;
+    wordleHelper(in, floating, dict, 1, length, &wordSet);
+    return wordSet;
 }
 
 // Define any helper functions here
-void findWord(int index,string& in, int dashes, std::string& floating, const std::set<std::string>& dict,
-const std::string& seeds, std::set<std::string>& possibleGusse)
+void wordleHelper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict, int pos, int len, std::set<std::string>* words)
 {
     
-		if(index == in.size())
-		{
-			 if(dict.find(in) != dict.end() && floating.empty())
-			 {
-				 possibleGusse.insert(in);
-			 }
-			 return;
-		}
-		if(dashes < floating.size())
-		{
-			return;
-		}
-		if (in[index] != '-'){
-			findWord(index+1, in, dashes, floating, dict, seeds, possibleGusse);
-			return;
-		}
-		// TODO: for loop over floats if dashes == floats.size() 
-		// instead of looping over alphabet
-		// if dashes == floaiting
-		  // loop through floating
-				// remove that letter from floating
-				// recurse
-				// add letter back
-			// return
-      
-	
-			char letter;
-			if(dashes == floating.size())
-			{
-				// loop i over 0->size float
-				std::string float_copy = floating;
-				std::string used;
-				for(int i = 0;i <floating.size();i++)
-				{
-					letter = floating[i];
-					if (used.find(letter) == std::string::npos){
-						used.push_back(letter);
-						in[index] = letter;
-						floating.erase(i, 1);
-						findWord(index+1, in, dashes - 1, floating, dict, seeds, possibleGusse);
-						floating = float_copy;
-					}
-				}
-				in[index] = '-';
+    if (pos > len)
+    {
+        if (words->find(in) != words->end())
+        {
+            return;
+        }
+        
+        if (floating.size() == 0)
+        {
+            if (dict.find(in) != dict.end())
+            {
+                words->insert(in);
+                return;
+            }
+        }
         return;
-			}
-
-    //have if statement for check for if you have more  -- than floating cha 
-
-		for(int i = 0; i < seeds.size(); i++)
-		{
-			char letter = seeds[i];
-			in[index] = seeds[i];
-				size_t f_index = floating.find(in[index]);
-				if(f_index != std::string::npos)
-				{
-					floating.erase(f_index, 1);
-					findWord(index+1, in, dashes - 1, floating, dict, seeds, possibleGusse);
-					floating.push_back(letter);
-				}
-				else
-				{
-					findWord(index+1, in, dashes - 1, floating, dict, seeds, possibleGusse);
-				}
-				// set in[index] back to "-"
-				in[index] = '-';
-			}
+    }
+    
+    if (in[pos - 1] != '-')
+    {
+        wordleHelper(in, floating, dict, pos + 1, len, words);
+        return;
+    }
+    
+    int count = 0;
+    for (int i = pos - 1; i < len; i++)
+    {
+        if (in[i] == '-')
+        {
+            count += 1;
+        }
+    }
+    
+    if (count < floating.size())
+    {
+        return;
+    }
+    
+    if (count == floating.size())
+    {
+        string newIn;
+        for (int i = 0; i < floating.size(); i++)
+        {
+            string floatNew = floating;
+            newIn = in;
+            newIn[pos - 1] = floatNew[i];
+            floatNew.erase(floatNew.begin() + i);
+            wordleHelper(newIn, floatNew, dict, pos + 1, len, words);
+        }
+        newIn[pos - 1] = '-';
+    }
+    else
+    {
+        std::set<char> floatLetters;
+        string newIn;
+        if (floating.size() != 0)
+        {
+            for (int i = 0; i < floating.size(); i++)
+            {
+                string floatNew = floating;
+                newIn = in;
+                newIn[pos - 1] = floatNew[i];
+                floatNew.erase(floatNew.begin() + i);
+                floatLetters.insert((char)(i + 97));
+                wordleHelper(newIn, floatNew, dict, pos + 1, len, words);
+            }
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (floatLetters.find((char)(i + 97)) != floatLetters.end())
+            {
+                continue;
+            }
+            else
+            {
+                newIn = in;
+                newIn[pos - 1] = (char)(i + 97);
+                wordleHelper(newIn, floating, dict, pos + 1, len, words);
+            }
+        }
+        newIn[pos - 1] = '-';
+    }
 }
